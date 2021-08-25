2fc1bb3a7ac84021a7175dd2fdfa78e550c1cc08
Tim Kallman <tim.kallman@gmail.com>
2021-08-25 13:50:47 -0400

    f

--- commands/yo.js
+++ commands/yo.js
@@ -1,6 +1,6 @@
-const { SlashCommandChannelOption } = require('@discordjs/builders');
-const Discord = require('discord.js'); 
-const client = new Discord.Client();
+// const { SlashCommandChannelOption } = require('@discordjs/builders');
+// const Discord = require('discord.js'); 
+// const client = new Discord.Client();
 // const apiFM = "22df4bf82525784ac8e983c4e67379a0";
 // const lastURL = 'http://ws.audioscrobbler.com/2.0';
 // const topTracks = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=rj&api_key=22df4bf82525784ac8e983c4e67379a0';
