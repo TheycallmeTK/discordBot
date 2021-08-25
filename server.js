b4be3a195c3b5f9e65bbce0e676e0d4a4b706f1d
Tim Kallman <tim.kallman@gmail.com>
2021-08-25 13:51:50 -0400

    test

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const client = new Discord.Client();
 const token = 'ODc5ODI0ODcyNTc3MDQwNDg2.YSVWww.8DEf0jNyjSTILvIUq8l1gAVi6XU';
 var cmd;
 
-client.on('message', message=>{
-    var msg = message.content;
-    var author = message.author
-    if(msg.startsWith(prefix, 0)){
-        console.log("yes prefix");
-        cmd = message.content.slice(prefix.length).trim();
+// client.on('message', message=>{
+//     var msg = message.content;
+//     var author = message.author
+//     if(msg.startsWith(prefix, 0)){
+//         console.log("yes prefix");
+//         cmd = message.content.slice(prefix.length).trim();
 
-        try{
-            var commandFile = require(`./commands/${cmd}.js`);
-            commandFile.run(client, message, args);
-        } catch(e){
+//         try{
+//             var commandFile = require(`./commands/${cmd}.js`);
+//             commandFile.run(client, message, args);
+//         } catch(e){
             
-        } finally{
-            console.log("command completed");
-        }
-    }
+//         } finally{
+//             console.log("command completed");
+//         }
+//     }
     
-});
+// });
 
 
 client.login(token);
